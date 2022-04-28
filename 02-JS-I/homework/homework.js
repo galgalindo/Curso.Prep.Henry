2a1d0c80684af08c5b574cbe3f6cb29dcc5c991b
galgalindo <104107453+galgalindo@users.noreply.github.com>
2022-04-28 17:42:26 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -62,7 +62,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-
+  
 }
 
 function tienenMismaLongitud(str1, str2) {
